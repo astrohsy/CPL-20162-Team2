19652350e4f237f9969bf2e21108c57958ceeda0
kshy9598 <kshy9598@naver.com>
2016-09-21 04:20:30 +0900

    EDSDK 변경

--- camera/camera/camera/camera.cpp
+++ camera/camera/camera/camera.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
-#include "take_picture.cpp"
+#include <Windows.h>
 
 using namespace std;
 
 int main()
 {
+	/*auto edsdkdll = LoadLibrary(TEXT("EDSDK.dll"));
+	if (edsdkdll == nullptr) {
+		cout << "로드 실패";
+		return 0;
+	}
 
+	auto edsdklibdll = LoadLibrary(TEXT("EDSDKLib.dll"));
+	if (edsdklibdll == nullptr) {
+		cout << "로드 실패";
+		return 0;
+	}
 
+	FreeLibrary(edsdkdll);
+	FreeLibrary(edsdklibdll);*/
 }
