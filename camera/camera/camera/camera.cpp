2099a68423f30a5ab61c4fe4c3029e25f35632a3
kshy9598 <kshy9598@naver.com>
2016-09-27 01:57:59 +0900

    camera sdk경로 추가

--- camera/camera/camera/camera.cpp
+++ camera/camera/camera/camera.cpp
@@ -5,18 +5,4 @@ using namespace std;
 
 int main()
 {
-	/*auto edsdkdll = LoadLibrary(TEXT("EDSDK.dll"));
-	if (edsdkdll == nullptr) {
-		cout << "로드 실패";
-		return 0;
-	}
-
-	auto edsdklibdll = LoadLibrary(TEXT("EDSDKLib.dll"));
-	if (edsdklibdll == nullptr) {
-		cout << "로드 실패";
-		return 0;
-	}
-
-	FreeLibrary(edsdkdll);
-	FreeLibrary(edsdklibdll);*/
 }
