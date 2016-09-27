073fc5a60b7370c6d31ec8901727ca7ac519f2ca
kshy9598 <kshy9598@naver.com>
2016-09-27 21:14:20 +0900

    camera 연결

--- camera/camera/camera/camera.cpp
+++ camera/camera/camera/camera.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
-#include <Windows.h>
+#include <string>
+
+#include "EDSDK.h"
+#include "EDSDKTypes.h"
+#include "CameraModel.h"
+#include "CameraModelLegacy.h"
+#include "CameraController.h"
+#include "CameraEventListener.h"
+
+#include "CameraControl.h"
 
 using namespace std;
 
+void take_picture(CCameraControlApp theApp){
+
+
+}
+
 int main()
 {
+	CCameraControlApp theApp;
+	theApp.InitInstance();
+
+	string doing;
+
+	while (1){ 
+		cin >> doing;
+
+		if (doing == "t"){
+			theApp._controller
+		}
+
+		else if (doing == "z")
+			break;
+	}
+
+	theApp.release();
 }
